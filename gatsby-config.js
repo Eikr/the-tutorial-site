5f5988f4e3817b2218729449b06a70c654826f29
Eikr <118434213+Eikr@users.noreply.github.com>
2022-11-18 14:10:56 +0100

    tutorial part 3.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `The Tutorial Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
