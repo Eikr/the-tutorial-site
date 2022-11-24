4270accefb965770381662db8aa364a451ba3792
Eikr <118434213+Eikr@users.noreply.github.com>
2022-11-24 13:39:15 +0100

    Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,21 @@
 
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-});
+})
 
 module.exports = {
   siteMetadata: {
-    title: `The Tutorial Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "The Tutorial Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
